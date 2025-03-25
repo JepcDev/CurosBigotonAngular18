fc945ec615c70b20843cbade156db048ca139106
JepcDev <joseenrique.pc21@gmail.com>
2025-03-25 15:30:46 -0400

    build: add initial configuration project

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,14 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { ApplicationConfig, provideExperimentalZonelessChangeDetection, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration()]
+  providers: [
+    provideExperimentalZonelessChangeDetection(),//ya no usamos zonejs
+    provideRouter(routes,withComponentInputBinding()),//withComponentInputBinding => queres el ID => perfecto es como si fuera un @Input() mas de mi componente
+    provideHttpClient(withFetch()), //permite hacer peticiones y (withFetch)le decimos a angular que ahora a la hora de hacer peticiones lo realice por meedio de fetch
+    provideClientHydration()]
 };
